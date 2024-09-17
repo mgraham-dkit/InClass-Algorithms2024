7cb1ec41be6d48273381fd99c718382804ec4cb3
michelle <michelle.graham@dkit.ie>
2024-09-17 14:25:45 +0100

    Added linear search pseudocode

--- src/main/java/app/TestApplication.java
+++ src/main/java/app/TestApplication.java
@@ -1,11 +1,14 @@
 package app;
 import utils.ArrayManipulation;
 
+import java.util.Arrays;
+
 public class TestApplication {
     public static void main(String[] args) {
         int [] data = {12, 25, 326, 213, 7, 23, 12, 8, -1, 24};
 
         ArrayManipulation.multiplyByTwo(data);
-        System.out.println(data);
+        System.out.println(Arrays.toString(data));
+        ArrayManipulation.printArray(data);
     }
 }
--- src/main/java/utils/ArrayManipulation.java
+++ src/main/java/utils/ArrayManipulation.java
@@ -1,7 +1,10 @@
 package utils;
 
 public class ArrayManipulation {
-
+    /**
+     * Multiply the contents of each slot in an array by 2
+     * @param data The array to be edited
+     */
     public static void multiplyByTwo(int [] data){
         /*
             for each value in the array:
@@ -9,6 +12,12 @@ public class ArrayManipulation {
          */
         for(int i = 0; i < data.length; i++){
             data[i] = data[i] * 2;
+        }
+    }
+
+    public static void printArray(int [] data){
+        for (int i = 0; i < data.length; i++) {
+            System.out.println((i+1) + ") " + data[i]);
         }
     }
 
