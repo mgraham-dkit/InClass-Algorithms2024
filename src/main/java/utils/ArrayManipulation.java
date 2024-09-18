f4aafe2c3d32442d9a98e1dee152461fb2cf1f9c
michelle <michelle.graham@dkit.ie>
2024-09-18 14:58:25 +0100

    Added javadocs to: - multiplyByTwo() - printArray() - getPosition() - getCount() - getMax()

--- src/main/java/utils/ArrayManipulation.java
+++ src/main/java/utils/ArrayManipulation.java
@@ -15,12 +15,22 @@ public class ArrayManipulation {
         }
     }
 
+    /**
+     * Print the contents of the supplied array to the screen.
+     * @param data The array to be displayed.
+     */
     public static void printArray(int [] data){
         for (int i = 0; i < data.length; i++) {
             System.out.println((i+1) + ") " + data[i]);
         }
     }
 
+    /**
+     * Get the (first) position of the supplied element within the supplied array.
+     * @param nums The array to search.
+     * @param value The value to search for.
+     * @return The first position of the value within the array, or -1 if it was not found.
+     */
     public static int getPosition(int [] nums, int value){
         for (int i = 0; i < nums.length; i++) {
             if(value == nums[i]){
@@ -30,6 +40,12 @@ public class ArrayManipulation {
         return -1;
     }
 
+    /**
+     * Get the number of times the supplied value appears within the supplied array.
+     * @param nums The array to be searched.
+     * @param term The value to be counted.
+     * @return The number of times the term appears within the array.
+     */
     public static int getCount(int [] nums, int term){
         int count = 0;
 
@@ -41,6 +57,11 @@ public class ArrayManipulation {
         return count;
     }
 
+    /**
+     * Gets the maximum value within the supplied array.
+     * @param nums The array to be searched.
+     * @return The maximum value found within the array.
+     */
     public static int getMax(int [] nums){
         int max = Integer.MIN_VALUE;
 
